d4c7a11fd03b4c3af1ba9a2017ca1da9726b0c4f
Yuri <yuri-user@proton.me>
2025-08-20 19:13:18 -0300

    feat(account handler): Add Login and Register forms

--- seu_condominio/src/app/app.routes.ts
+++ seu_condominio/src/app/app.routes.ts
@@ -1,3 +1,18 @@
-import { Routes } from '@angular/router';
+import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from '@angular/core';
 
-export const routes: Routes = [];
+import { Home } from './home/home';
+import { Register } from './register/register';
+import { Login } from './login/login';
+
+export const routes: Routes = [
+  { path: '', component: Home },
+  { path: 'register', component: Register },
+  { path: 'login', component: Login },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}
