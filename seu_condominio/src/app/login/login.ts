519f987f7dc9fc717de6633fa032916459fc5e7f
Yuri <yuri-user@proton.me>
2025-08-22 10:24:30 -0300

    Auth(login, register): Add Auth system logic

--- seu_condominio/src/app/login/login.ts
+++ seu_condominio/src/app/login/login.ts
@@ -1,9 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthService } from '../auth-service';
+import { HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  standalone: true,
+  imports: [FormsModule, HttpClientModule],
   templateUrl: './login.html',
   styleUrl: './login.css',
 })
-export class Login {}
+export class Login {
+  email = '';
+  password = '';
+
+  private authService = inject(AuthService);
+  private router = inject(Router);
+
+  onLogin(): void {
+    console.log('Attempting login with:', this.email);
+
+    this.authService.login(this.email, this.password).subscribe({
+      next: (response) => {
+        console.log('Login successful!', response);
+        window.location.href = '/';
+      },
+      error: (error) => {
+        console.error('Login failed!', error);
+      },
+    });
+  }
+}
--- seu_condominio/src/app/register/register.ts
+++ seu_condominio/src/app/register/register.ts
@@ -1,9 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthService } from '../auth-service';
+import { HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-register',
-  imports: [],
+  standalone: true,
+  imports: [FormsModule, HttpClientModule],
   templateUrl: './register.html',
   styleUrl: './register.css',
 })
-export class Register {}
+export class Register {
+  name = '';
+  email = '';
+  password = '';
+  password_confirm = '';
+
+  private authService = inject(AuthService);
+  private router = inject(Router);
+
+  onRegister(): void {
+    if (this.password !== this.password_confirm) {
+      console.error('Registration failed: Passwords do not match!');
+      return;
+    }
+
+    console.log('Attempting to register with:', this.email);
+
+    this.authService.register(this.name, this.email, this.password).subscribe({
+      next: (response) => {
+        console.log('Register successful!', response);
+        window.location.href = '/';
+      },
+      error: (error) => {
+        console.error('Register failed!', error);
+      },
+    });
+  }
+}
