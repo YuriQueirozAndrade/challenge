0e7869bca8dfd8fb4b2fb06d81c499025ab1155e
Yuri <yuri-user@proton.me>
2025-08-22 10:32:01 -0300

    Add(task): Add a task board component

--- seu_condominio/src/app/task-board/task-board.ts
+++ seu_condominio/src/app/task-board/task-board.ts
@@ -1,28 +1,42 @@
-import { Component, OnInit, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { GetTasksService } from '../get-tasks';
+import { ApiHandler } from '../api-handler';
 import { Task } from '../task/task';
 import { ITask } from '../ITask';
+import { Subscription } from 'rxjs';
+import { Component, OnInit, OnDestroy, inject, ChangeDetectorRef } from '@angular/core';
 
 @Component({
   selector: 'app-task-board',
   standalone: true,
   imports: [CommonModule, Task],
-  template: `
-    <h2>Project Tasks</h2>
-    <div class="task-list">
-      <app-task *ngFor="let task of tasks" [task]="task"> </app-task>
-    </div>
-  `,
+  templateUrl: './task-board.html',
   styleUrl: './task-board.css',
 })
-export class TaskBoard implements OnInit {
-  private getTasksService = inject(GetTasksService);
+export class TaskBoard implements OnInit, OnDestroy {
+  private api = inject(ApiHandler);
+  private cdr = inject(ChangeDetectorRef);
   tasks: ITask[] = [];
 
+  private taskSubscription!: Subscription;
+
   ngOnInit(): void {
-    this.getTasksService.getTasks().subscribe((tasks: ITask[]) => {
-      this.tasks = tasks;
+    this.fetchTasks();
+    this.taskSubscription = this.api.refresh$.subscribe(() => {
+      this.fetchTasks();
     });
+  }
+
+  fetchTasks(): void {
+    this.api.getAllTask().subscribe((tasks: ITask[]) => {
+      this.tasks = tasks;
+      console.log(this.tasks);
+      this.cdr.detectChanges();
+    });
+  }
+
+  ngOnDestroy(): void {
+    if (this.taskSubscription) {
+      this.taskSubscription.unsubscribe();
+    }
   }
 }
