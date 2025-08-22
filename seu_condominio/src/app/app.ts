2abef5036955a953996936febbb54922e3fbf186
Yuri <yuri-user@proton.me>
2025-08-22 10:22:11 -0300

    Update(app, index): Update index and app html, css, ts

--- seu_condominio/src/app/app.ts
+++ seu_condominio/src/app/app.ts
@@ -1,12 +1,31 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, signal, inject, OnInit } from '@angular/core';
+import { RouterOutlet, Router } from '@angular/router';
+import { AuthService } from './auth-service';
+import { SessionStorageHandler } from './session-storage-handler';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
-export class App {
+export class App implements OnInit {
   protected readonly title = signal('seu_condominio');
+  private auth = inject(AuthService);
+  private router = inject(Router);
+  private session = inject(SessionStorageHandler);
+  user_name: string | null = null;
+  ngOnInit(): void {
+    const authData = this.session.getAuthData();
+    if (authData && authData.user) {
+      this.user_name = authData.user.name;
+    }
+  }
+
+  logout(): void {
+    this.auth.logout();
+    window.location.reload();
+  }
 }
